aeaca52142da6432c149b05ec4033ca610933246
ihtesham <mohammedihtesham21@gmail.com>
2023-05-13 19:20:30 +0530

    completed app

--- app.js
+++ app.js
@@ -1,34 +1,58 @@
-const billAmount = document.querySelector("#bill-amount")
-const cashGiven = document.querySelector("#cash-given")
-const check = document.querySelector("#btn-check")
-const errorMessage = document.querySelector("#error-message")
-const noteValue = [2000,500,100,20,10,5,1];
-const noOfNotes = document.querySelectorAll(".notes");
-check.addEventListener("click", function process(){
-    errorMessage.style.display = "none";
-    if(billAmount.value > 0){
-        if(Number(cashGiven.value) >= Number(billAmount.value)){
-            const amountReturn = cashGiven.value - billAmount.value;
-            changeReturn(amountReturn);
-            
-        }else{
-            
-            showMessage("Cash given is less than bill amount")
-        }
-    }else{
-        
-        showMessage("Enter value greater than 0")
+let billAmt = document.querySelector("#bill-amt")
+let nextBtn = document.querySelector("#next");
+let cashAmt = document.querySelector("#cash-amt");
+let returnChangeBtn = document.querySelector("#retrn");
+let cashDisplay = document.querySelector(".cash")
+let errorMsg = document.querySelector(".error");
+let output = document.querySelector(".output");
+let notes = document.querySelectorAll(".noofNotes");
+let value = [2000,500,100,50,20,10,5,2,1];
+
+function hide(){
+    cashDisplay.style.display = "none";
+    output.style.display = "none";
+    errorMsg.style.display = "none";
+}
+
+function show(){
+    cashDisplay.style.display = "block";
+}
+function showError(text){
+    errorMsg.style.display = "block";
+    errorMsg.innerText = text;
+}
+nextBtn.addEventListener("click",()=>{
+    hide();
+    if(Number(billAmt.value) >0){
+        show();
+    }
+    else{
+        showError("Enter valid bill amount");
     }
 })
 
-function changeReturn(amountReturn){
-    for(let i = 0; i<noteValue.length ; i++){
-        const noNote = Math.trunc(amountReturn/noteValue[i]);
-        amountReturn = amountReturn % noteValue[i];
-        noOfNotes[i].innerText = noNote;
+returnChangeBtn.addEventListener("click",()=>{
+    if(Number(billAmt.value) > 0 && Number(cashAmt.value) > 0 && Number(billAmt.value) < Number(cashAmt.value))
+    {
+        let returnAmout = cashAmt.value - billAmt.value;
+        output.style.display = "block";
+        calculate(returnAmout);
+    }
+    else{
+        showError("Enter valid cash amount");
+    }
+})
+
+function calculate(amount){
+    for(let i=0; i<value.length;i++){
+        let nt = Math.trunc(amount/value[i]);
+        amount = amount%value[i];
+        if(nt == 0){
+            notes[i].innerText = "";
+        }
+        else{
+            notes[i].innerText = nt;
+        }
     }
 }
-function showMessage(msg){
-    errorMessage.style.display = "block";
-    errorMessage.innerText = msg;
-}
+hide();
